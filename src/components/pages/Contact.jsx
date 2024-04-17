894765cad5c7589616886dbc4a8c129a6e04b303
Joon Yoon <myexistance@HOTMAIL.COM>
2024-04-18 10:59:37 +1200

    work on the contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,17 +1,47 @@
+// import React from "react";
+
+// export const Contact = () => {
+//   return (
+//         <div style={{ display: 'flex' }}>
+//             <div style={{ flex: 1 }}>
+//                 {/* Map goes here */}
+//                 <img src="map.jpg" alt="Map" style={{ width: '100%', height: 'auto' }} />
+//             </div>
+//             <div style={{ flex: 1, padding: '0 20px' }}>
+//                 {/* Paragraph goes here */}
+//                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+//             </div>
+//         </div>
+//     );
+// };
+
 import React from "react";
 
 export const Contact = () => {
   return (
-        <div style={{ display: 'flex' }}>
-            <div style={{ flex: 1 }}>
-                {/* Map goes here */}
-                <img src="map.jpg" alt="Map" style={{ width: '100%', height: 'auto' }} />
-            </div>
-            <div style={{ flex: 1, padding: '0 20px' }}>
-                {/* Paragraph goes here */}
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
-            </div>
-        </div>
-    );
-};
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ flex: 1 }}>
+        {/* Map goes here */}
+        <img src="map.jpg" alt="Map" style={{ width: '100%', height: 'auto' }} />
+      </div>
+      <div style={{ flex: 1, padding: '20px', textAlign: 'center' }}>
+        {/* Paragraph goes here */}
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+      </div>
 
+      {/* Media query for responsiveness */}
+      <style>
+        {`
+          @media only screen and (min-width: 600px) {
+            .contact-wrapper {
+              flex-direction: row;
+            }
+            .contact-wrapper > div {
+              flex: 1;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
+};
