18e8e64c8087bc23c995ed728b6ab7f58f3bda22
Joon Yoon <myexistance@HOTMAIL.COM>
2024-04-18 17:24:43 +1200

    map iframe and offering added

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -21,12 +21,23 @@ export const Contact = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <div style={{ flex: 1 }}>
-        {/* Map goes here */}
-        <img src="map.jpg" alt="Map" style={{ width: '100%', height: 'auto' }} />
+        <br />
+        <p style={{ textAlign: 'center' }}>MAP: 88 The Parade, Island Bay, Wellington 6023</p>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4609.779165050718!2d174.77022562713333!3d-41.33224884848255!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38baad534e9011%3A0xfcb0c92cd1b4bb49!2s88%20The%20Parade%2C%20Island%20Bay%2C%20Wellington%206023!5e0!3m2!1sen!2snz!4v1713416854861!5m2!1sen!2snz"
+          width="100%"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
+      <br />
+      <br />
       <div style={{ flex: 1, padding: '20px', textAlign: 'center' }}>
         {/* Paragraph goes here */}
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+        <p>OFFERING: ASB kjhbsdjbfla</p>
       </div>
 
       {/* Media query for responsiveness */}
--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,67 +1,45 @@
 import React from "react";
-import YouTubeEmbed from './YouTubeEmbed';
+import YouTubeEmbed from './YouTubeEmbed'; // Assuming you have a component for embedding YouTube videos
 
 export const Services = () => {
   return (
     <>
-      
       <div style={{ 
-    width: '100%'
-    // display: 'flex', 
-    // justifyContent: 'center', 
-    // alignItems: 'center'
-  }}>
-        <br />
-        <br />
-        <br />
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+      }}>
         <p style={{ 
           textAlign: "left",
           padding: "0 80px"
-       }}>
+        }}>
+          {/* Some content */}
         </p>
       </div>
-      <br />
       <div>
         <h2 style={{ 
           textAlign: "center",
           padding: "0 80px"
-       }}>주일 예배시간</h2>
+        }}>주일 예배시간</h2>
       </div>
-      <br />
-      <div style={{textAlign: "center",
-          padding: "0 50px"}}>
+      <div style={{ textAlign: "center", padding: "0 50px" }}>
         1부: 오후 1:30 Island Bay Church <br />
         2부: 오후 2:30 교제 모임 <br />
         주일학교: 오후 12:40 본당
       </div>
-      <br />
       <div>
-    <h2 style={{textAlign: "center"}}>주일예배 
-    YouTube</h2>
-    <br />
-    <div style={{ 
-    width: '100%',
-    display: 'flex', 
-    justifyContent: 'center', 
-    alignItems: 'center'
-  }}>
-    <YouTubeEmbed embedId="your-youtube-video-id" />
-  </div>
-            
-          
-    
-  </div>
-      <br />
-      <div>
-        
+        <h2 style={{ textAlign: "center" }}>주일예배 YouTube</h2>
+        <div style={{ 
+          width: '100%',
+          height: '100%',
+          padding: '0 20px', // Adjust padding as needed
+          display: 'flex',
+          justifyContent: 'center', 
+          alignItems: 'center'
+        }}>
+          <YouTubeEmbed embedId="your-youtube-video-id" />
+        </div>
       </div>
-      <br />
-      <div>
-        
-      
-      </div>
-      <br />
-      
     </>
   );
 };
