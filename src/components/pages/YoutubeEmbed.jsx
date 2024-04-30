f7572d3d92c1d259843340b5d1314bdea2e14080
Joon Yoon <myexistance@HOTMAIL.COM>
2024-04-30 16:01:28 +1200

    .env file added and hid the the api.

--- src/components/pages/YoutubeEmbed.jsx
+++ src/components/pages/YoutubeEmbed.jsx
@@ -1,22 +1,4 @@
-// import React from 'react';
 
-
-// const YouTubeEmbed = ({ embedId }) => (
-//   <div className="video-responsive" >
-//     <iframe  
-//   width="100%" 
-//   height="100%" 
-//   src="https://www.youtube.com/embed/xY0znjOBY9w?si=pmaK5fk0HsZAvdZo" 
-//   title="YouTube video player" 
-//   frameborder="0" 
-//   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-//   referrerpolicy="strict-origin-when-cross-origin" 
-//   allowfullscreen
-// ></iframe>
-//   </div>
-// );
-
-// export default YouTubeEmbed;
 import React, { useState, useEffect } from 'react';
 
 const YouTubeEmbed = () => {
@@ -26,7 +8,9 @@ const YouTubeEmbed = () => {
     // Function to fetch the latest video ID
     const fetchLatestVideoId = async () => {
       try {
-        const response = await fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyBJNywBHT3wq9PJQZIZDkH2i4tv9DCAJYs&channelId=UC-5KZRaltWluIGIEZCPle7w&part=snippet&order=date&maxResults=1');
+        // google API does not seem to work, needs a fix...
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&channelId=UC-5KZRaltWluIGIEZCPle7w&part=snippet&order=date&maxResults=1`);
+
         const data = await response.json();
         // Assuming the response contains the latest video ID
         const latestId = data.items[0].id.videoId;
