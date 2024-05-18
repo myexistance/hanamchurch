7a0283d7451804e9adf10f1151ba2d206f6b7a31
Joon Yoon <myexistance@HOTMAIL.COM>
2024-05-18 14:46:02 +1200

    html body amended

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,47 @@ import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { About, Contact, Home, Services, Column} from "./components/pages";
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    height: 100%;
+    margin: 0;
+    overflow-x: hidden;
+    font-family: 'Noto Serif KR', serif;
+    background-color: #fefefe;
+  }
+  
+  #root {
+    height: 100%;
+  }
+`;
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
 
 function App() {
+  
   return (
+    <>
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/column" element={<Column />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <AppWrapper>
+        <Navbar />
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/column" element={<Column />} />
+          <Route path="/contact" element={<Contact />} />
+          </Routes>
       <Footer />
+      </AppWrapper>
+      
     </div>
+    </>
   );
 }
 
