2ff73ab2fa4265b75517a473e236777fb5feeaf5
Joon Yoon <myexistance@HOTMAIL.COM>
2024-12-06 00:45:51 +1300

    footer position and weekly column added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 export function Footer() {
   return (
-    <footer style={{ textAlign: 'right',paddingRight: "20px" }}>
+    <footer style={{ position: 'fixed',
+  bottom: '0',
+  right: '0',
+  paddingRight: '20px',
+  fontSize: '16px'}}>
       <p style={{ fontSize: "16px" }}>&copy; {new Date().getFullYear()} Joon. All rights reserved.</p>
     </footer>
   );
