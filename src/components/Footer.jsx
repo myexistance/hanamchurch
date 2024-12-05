54f46113b994c6c83d6797545114bdffa2d44408
Joon Yoon <myexistance@HOTMAIL.COM>
2024-12-06 00:57:34 +1300

    footer position reverted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 export function Footer() {
   return (
-    <footer style={{ position: 'fixed',
-  bottom: '0',
-  right: '0',
-  paddingRight: '20px',
-  fontSize: '16px'}}>
+    <footer style={{ textAlign: 'right',paddingRight: "20px"}}>
       <p style={{ fontSize: "16px" }}>&copy; {new Date().getFullYear()} Joon. All rights reserved.</p>
     </footer>
   );
