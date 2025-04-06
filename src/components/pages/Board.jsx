2c5a5f8742e929f0c19f1d0067ddbf2e75d4ee52
Joon Yoon <myexistance@HOTMAIL.COM>
2025-04-06 16:01:11 +1200

    april board replaced

--- src/components/pages/Board.jsx
+++ src/components/pages/Board.jsx
@@ -1,6 +1,41 @@
-import React from "react";
-import newsimage from '../../asset/seminar.png';
+// import React from "react";
+// import newsimage from '../../asset/april.png';
 
+
+// export const Board = () => {
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         marginTop: "0",
+//         padding: "0",
+//         backgroundColor: "#fff",
+//         borderRadius: "10px",
+//         boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+//         width: "100vw",
+//         height: "100vh",
+//         overflow: "auto",
+//       }}
+//     >
+//       <img
+//         src={newsimage}
+//         style={{
+//           width: "90%",
+//           height: "auto",
+//           display: "block",
+//           objectFit: "contain",
+//         }}
+//         alt="A visually appealing board"
+//         loading="lazy"
+//       />
+//     </div>
+//   );
+// };
+
+import React from "react";
+import newsimage from '../../asset/april.png';
 
 export const Board = () => {
   return (
@@ -9,28 +44,28 @@ export const Board = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        marginTop: "0",
-        padding: "0",
+        margin: 0,
+        padding: 0,
         backgroundColor: "#fff",
         borderRadius: "10px",
         boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
         width: "100vw",
-        height: "100vh",
+        height: "100%",
+       
         overflow: "auto",
       }}
     >
       <img
         src={newsimage}
+        alt="April news board"
         style={{
-          width: "90%",
+          maxWidth: "90%",
           height: "auto",
-          display: "block",
           objectFit: "contain",
+          borderRadius: "8px",
         }}
-        alt="A visually appealing board"
         loading="lazy"
       />
     </div>
   );
 };
-
